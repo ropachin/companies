39e93537fdee5e46c62a6ba128002a5c3b8bb6f9
ropachin <ropachin@gmail.com>
2023-02-21 20:07:40 +0300

    unit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <?php require 'inc/head_inc' ?>
+    <title>Тестовое задание</title>
+</head>
+
+<body>
+    <?php require 'inc/header' ?>
+</body>
+
+</html>
