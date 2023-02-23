7e5ea7e95fde5f4f41636bd9c4d07451b6a54410
ropachin <ropachin@gmail.com>
2023-02-23 04:18:44 +0300

    start auth

---
+++ auth.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <?php require 'inc/head_inc' ?>
+    <title>Тестовое задание</title>
+    <link rel="stylesheet" href="auth.css">
+    <script src="auth.js" defer></script>
+</head>
+
+<body>
+    <?php require 'inc/header' ?>
+    <main id="auth-main">
+        <?php if (isset($_GET['reg'])) : ?>
+            <!-- Если нужна форма регистрации -->
+            <form class="auth-form" method="post" action="/server/reg.php">
+                <h1>Регистрация</h1>
+                <div class="auth-inputs-parent">
+                    <input class="default-input-text" type="text" name="name" require minlength="8" maxlength="50" placeholder="ФИО">
+                    <input class="default-input-text" type="email" name="email" require minlength="8" maxlength="50" placeholder="E-MAIL">
+                    <input class="default-input-text" type="password" name="password" require minlength="8" maxlength="32" name="password" id="reg-pass-1" placeholder="Пароль">
+                    <input class="default-input-text" type="password" require minlength="8" maxlength="32" name="password" id="reg-pass-2" placeholder="Повторите пароль">
+                    <input class="default-btn auth-btn" type="submit" value="Зарегестрироваться">
+                </div>
+            </form>
+        <?php else : ?>
+            <!-- Если нужна форма регистрации авторизации -->
+        <?php endif ?>
+    </main>
+</body>
+
+</html>
--- server/set_comment.php
+++ server/set_comment.php
@@ -1,4 +1,5 @@
 <?php
 // Скрипт, записывает коментарий пользователя в базу данных
 include '../inc/db_connect';
-echo $mysql->query("INSERT INTO `comments`(`company_id`, `user_id`, `visibility`, `text`) VALUES ('{$_POST['company_id']}','{$_POST['user_id']}','{$_POST['visibility']}','{$_POST['text']}')");
+$date = date('Y:m:d');
+echo $mysql->query("INSERT INTO `comments`(`company_id`, `user_id`, `visibility`, `text`,`date`) VALUES ('{$_POST['company_id']}','{$_POST['user_id']}','{$_POST['visibility']}','{$_POST['text']}','$date')");
